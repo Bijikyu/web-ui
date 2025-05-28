d940a3c7316a98a638f8ae584adc960724cd15c0
Q <qontact@protonmail.com>
2025-05-27 23:02:30 -0700

    chore: dedupe context imports

--- src/browser/custom_browser.py
+++ src/browser/custom_browser.py
@@ -9,9 +9,8 @@ from patchright.async_api import (
     async_playwright,
 )
 from browser_use.browser.browser import Browser, IN_DOCKER
-from browser_use.browser.context import BrowserContext, BrowserContextConfig
-from patchright.async_api import BrowserContext as PlaywrightBrowserContext
-import logging
+from browser_use.browser.context import BrowserContext, BrowserContextConfig  # deduped duplicate import
+import logging  # removed extra PlaywrightBrowserContext import
 
 from browser_use.browser.chrome import (
     CHROME_ARGS,
@@ -20,7 +19,6 @@ from browser_use.browser.chrome import (
     CHROME_DOCKER_ARGS,
     CHROME_HEADLESS_ARGS,
 )
-from browser_use.browser.context import BrowserContext, BrowserContextConfig
 from browser_use.browser.utils.screen_resolution import get_screen_resolution, get_window_adjustments
 from browser_use.utils import time_execution_async
 import socket
