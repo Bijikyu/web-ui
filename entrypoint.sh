4737b575e20b11ce9df18f10292858122262c516
Q <qontact@protonmail.com>
2025-05-29 23:10:52 -0700

    docs: add explanations for startup scripts

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-# Start supervisord in the foreground to properly manage child processes
-exec /usr/bin/supervisord -n -c /etc/supervisor/conf.d/supervisord.conf
+# Start supervisord in the foreground so Docker can monitor it    # ensures container shuts down cleanly when supervisord exits
+exec /usr/bin/supervisord -n -c /etc/supervisor/conf.d/supervisord.conf    # launch supervisor with provided config
